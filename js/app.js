b2daebbf8b3e43f14cb967969013cc7a3148be8e
shihab00027 <shihabhasan981@gmail.com>
2022-09-03 00:16:31 +0600

    News Category Added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log('hello');
+const loadCaregory = () => {
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+    .then(response => response.json())
+    .then(data => displayCategory(data.data.news_category))
+}
+
+const displayCategory = (data) => {
+    // console.log(data);
+    const categoryContainer = document.getElementById('category-container');
+    data.forEach(newsData => {
+        console.log(newsData);
+        const categoryDiv = document.createElement('div');
+        categoryDiv.innerText = newsData.category_name;
+        categoryContainer.appendChild(categoryDiv);
+    })
+    
+}
+loadCaregory();
