82c570be88827b21b4fb825d171862816a9c8924
shihab00027 <shihabhasan981@gmail.com>
2022-09-03 06:22:47 +0600

    Spinner added

--- js/app.js
+++ js/app.js
@@ -7,45 +7,65 @@ const loadCaregory = () => {
 const displayCategory = (data) => {
     // console.log(data);
     const categoryContainer = document.getElementById('category-container');
+    categoryContainer.textContent = '';
     data.forEach(newsData => {
         // console.log(newsData);
         const categoryDiv = document.createElement('div');
         categoryDiv.innerHTML = `
         <div onclick="CategoryNewsId('${newsData.category_id}')">${newsData.category_name}</div>
-        `
+        `;
         categoryContainer.appendChild(categoryDiv);
-    })
-}
 
+    })
+
+}
+const spinner = document.getElementById('show-spinner');
 const CategoryNewsId = (newsId) => {
     const url = `https://openapi.programming-hero.com/api/news/category/${newsId}`
     fetch(url)
         .then(response => response.json())
         .then(data => displayNews(data.data))
+    spinner.classList.remove('hidden')
 }
 const displayNews = (newsData) => {
     const displayNews = document.getElementById('display-news');
     displayNews.textContent = '';
+    const notFound = document.getElementById('no-found')
+    // notFound.textContent = '';
+    const found = document.getElementById('found');
+    found.textContent = '';
+    if (newsData.length === 0) {
+        notFound.classList.remove('hidden')
+        found.classList.add('hidden')
+    }
+    else{
+        notFound.classList.add('hidden')
+        found.classList.remove('hidden')
+        console.log(newsData.length);
+       
+        const p = document.createElement('p')
+        // p.classList.add('font-bold text-2xl p-6 text-center')
+        p.innerText = `${newsData.length} News Are Found`;
+        found.appendChild(p);
+    }
     newsData.forEach(news => {
-        // console.log(news);
+        console.log(news);
         const newsDiv = document.createElement('div');
         newsDiv.innerHTML = `
     <div class="m-8">
         <div class="card card-side bg-base-100 shadow-xl">
-                <figure><img src="${news.thumbnail_url}" class="w-80 h-80 rounded-xl ml-10" alt="Movie"></figure>
-            <div class="card-body pl-10">
+        <img src="${news.thumbnail_url}" class="w-80 h-80 rounded-xl m-10" alt="Movie">
+            <div class="card-body pl-10 w-3/4">
                 <h2 class="card-title font-bold text-xl pt-10">${news.title}</h2>
-                <p class=" text-xl">${news.details.length > 300 ? news.details.slice(0, 300) + '...' : news.details}</p>
-                <div class="card-actions flex justify-between">
-                <div class="">
-                <div class="w-20 flex">
+                <p class=" text-xl pt-4">${news.details.length > 300 ? news.details.slice(0, 300) + '...' : news.details}</p>
+                <div class="card-actions flex justify-between items-center">
+                <div class="w-20 flex items-center">
                 <img class="rounded-full" src="${news.author.img}"/>
-                <p class="px-10 text-2xl font-bold">${news.author.name ? news.author.name : 'No Author'}</p>
+                <h3 class="px-10 text-2xl font-bold">${news.author.name ? news.author.name : 'No Data Found'}</h3>
                  </div>
-                </div>
-                 <div class="flex align-center">
+                 <div class="flex items-center">
                  <i class="fa-regular fa-eye fa-2xl"></i>
-                 <h3 class="px-6 font-bold text-2xl ">${news.total_view ? news.total_view + 'K' : '00'}</h3>
+                 <h3 class="px-6 font-bold text-2xl ">${news.total_view ? news.total_view + 'K' : 'No Data Found'}</h3>
                  </div>
                  <div>
                  <i class="fa-regular fa-star fa-2xl"></i>
@@ -64,8 +84,8 @@ const displayNews = (newsData) => {
 
         `;
         displayNews.appendChild(newsDiv);
-        // <a href="#my-modal-2" class="btn">open modal</a>
     })
+    spinner.classList.add('hidden')
 }
 
 const modalData = (idNews) => {
